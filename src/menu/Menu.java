4517f6121b73cc15adc462322bec1a9f507aefa0
Kovács Hunor Lajos <kovacshunor4@gmail.com>
2024-11-07 17:27:47 +0100

    basic menu skeleton

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,43 +1,57 @@
 package menu;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
 import snake.Main;
 
 public class Menu extends JPanel {
     public Menu() {
         setName("Menu");
-        setPreferredSize(new Dimension(400, 400));
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
 
+        JTextField input = new JTextField(20);
+        JPanel upper = new JPanel();
+        JLabel usernameLabel = new JLabel(Main.getUsername());
+        JButton okButton = new JButton("Set username");
+        okButton.addActionListener(ae -> {
+            String text = input.getText();
+            Main.setUsername(text);
+            if(!text.equals("")){
+                usernameLabel.setText(text);
+            }
+        });
+
+        upper.add(input);
+        upper.add(okButton);
+        upper.add(usernameLabel);
+
+        
         JButton button1 = new JButton("Play");
         JButton button2 = new JButton("Leaderboard");
         JButton button3 = new JButton("Exit");
-
-        button1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Main.init(new snake.Field());
-            }
-        });
-        button2.addActionListener(ae -> {
-            Main.init(new leaderboard.Leaderboard());
-        });
+        
+        button1.addActionListener(ae -> Main.init(new snake.Field()));
+        button2.addActionListener(ae -> Main.init(new leaderboard.Leaderboard()));
         button3.addActionListener(ae -> System.exit(0));
-
+        
         gbc.gridx = 0;
         gbc.gridy = 0;
-        add(button1, gbc);
+
+        add(upper);
 
         gbc.gridx = 0;
         gbc.gridy = 1;
-        add(button2, gbc);
+
+        add(button1, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 2;
+        add(button2, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
         add(button3, gbc);
         setVisible(true);
     }
--- src/snake/Main.java
+++ src/snake/Main.java
@@ -6,6 +6,7 @@ import menu.Menu;
 
 public class Main {
     private static JFrame frame;
+    private static String username = "sample_username";
 
     public static void init(JPanel panel) {
         if(frame != null){
@@ -23,6 +24,14 @@ public class Main {
         frame.pack();
         frame.setLocationRelativeTo(panel);
         frame.setVisible(true);
+    }
+
+    public static void setUsername(String name){
+        username = name;
+    }
+
+    public static String getUsername(){
+        return username;
     }
 
     public static void main(String... args) {
